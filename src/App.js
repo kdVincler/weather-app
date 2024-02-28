802be237bb2de73d57ec839fe2fd9a96ab973710
ec221017 <ec221017@qmul.ac.uk>
2024-02-28 22:25:56 +0000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      {/* Any component call has to be in this div!!!! VERY IMPORTANT */}
+      <h1>This is a skeleton file that was created by create-react-app and modified</h1>
+      <p>
+        I got rid of the placeholder text in App.js and the styling in App.css
+        <br />
+        !BUT!
+        <br />
+        if you want your actual react components to run here, then import them and call them in the div that this h1 and paragraph is. (I left a comment in the code too)
+        <br />
+      </p>
+    </div>
+  );
+}
+
+export default App;
